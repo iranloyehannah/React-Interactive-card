73df2c8c754e7b81e1f10295379c65cb12f6607a
Iranloye Hannah <iranloye8.hannah@gmail.com>
2025-08-07 16:26:10 +0100

    Build production version

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // if applicable
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
-  server: {
-    port: 3000,
-    open: true
-  }
+  plugins: [react(), tailwindcss()],
 })
