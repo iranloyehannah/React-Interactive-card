4f4037b8ba7a07c9812edfce83a55831689d8fab
Iranloye Hannah <iranloye8.hannah@gmail.com>
2025-08-07 16:49:49 +0100

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
       <section className="w-full max-h-[760px] lg:h-screen flex flex-col gap-10 lg:gap-0 lg:flex-row font-medium">
         <CardPreview cardData={submittedData} />
         {message ? (
-          <div className="w-full h-[350px] lg:w-[70%] lg:h-0 flex items-center flex-col justify-center gap-4">
+          <div className="w-full h-[350px] lg:w-[70%] lg:h-[750px] flex items-center flex-col justify-center gap-4">
             <img src={completeIcon} alt="completeIcon" />
             <p className="text-2xl">Thank You</p>
             <span>We added Your Card Details</span>
